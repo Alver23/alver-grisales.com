d37e8713c4a9013ffaea9517f0050c347efd5f74
Alver Alexander Grisales Ortega <viga.23@hotmail.com>
2016-04-21 20:36:55 -0500

    Se creo tab de CV

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,51 @@
 var express = require('express')
 
 var router = express.Router()
-
+var lenguages = [
+		{
+			name: "PHP",
+			level: 80,
+			description: "Probando men"
+		},
+		{
+			name: "jQuery",
+			level: 70,
+			description: "Probando men"
+		},
+		{
+			name: "HTML5",
+			level: "90",
+			description: "Probando men"
+		},
+		{
+			name: "CSS3",
+			level: "80",
+			description: "Probando men"
+		},
+		{
+			name: "Javascript",
+			level: "70",
+			description: "Probando men"
+		}
+	]
+var officeApplication = [
+	{
+		name: "MS Word",
+		level: 40,
+	},
+	{
+		name: "MS Excel",
+		level: 60,
+	},
+	{
+		name: "Powerpoint",
+		level: 40,
+	}
+]
 
 router.get('/', function (req, res) {
-	res.render('index',{ title: 'Alver Grisales Ortega'});
+	//var lenguages = [{ "name":"PHP", "level": "80" }, { "name": "jQuery", "level": "70" }]
+	res.render('index',{ title: 'Alver Grisales Ortega', lenguages : lenguages, officeApplication: officeApplication});
 })
 
 module.exports = router
