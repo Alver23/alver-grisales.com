348a3a940a617ebb953cbfebefa91f6304b7cf4f
Alver Grisales <viga.23@hotmail.com>
2016-04-21 22:16:12 -0500

    Se agrego carpeta models y se modifico el index de routes

---
+++ models/index.js
@@ -0,0 +1,94 @@
+var lenguages = [
+	{
+		name: "PHP",
+		level: 80,
+		description: "Probando men"
+	},
+	{
+		name: "jQuery",
+		level: 70,
+		description: "Probando men"
+	},
+	{
+		name: "HTML5",
+		level: "90",
+		description: "Probando men"
+	},
+	{
+		name: "CSS3",
+		level: "80",
+		description: "Probando men"
+	},
+	{
+		name: "Javascript",
+		level: "70",
+		description: "Probando men"
+	},
+	{
+		name: "React Js",
+		level: "30",
+		description: "Probando men"
+	},
+	{
+		name: "Polymer Js",
+		level: "30",
+		description: "Probando men"
+	},
+	{
+		name: "Python y Django",
+		level: "10",
+		description: "Probando men"
+	},
+	{
+		name: "Node Js",
+		level: "40",
+		description: "Probando men"
+	},
+	{
+		name: "Angular Js",
+		level: "20",
+		description: "Probando men"
+	},
+	{
+		name: "Grunt Js",
+		level: "30",
+		description: "Probando men"
+	},
+	{
+		name: "Gulp Js",
+		level: "40",
+		description: "Probando men"
+	}
+	,
+	{
+		name: "Browserify",
+		level: "40",
+		description: "Probando men"
+	},
+	{
+		name: "Jade Templates",
+		level: "50",
+		description: "Probando men"
+	},
+	{
+		name: "Express Js",
+		level: "30",
+		description: "Probando men"
+	}
+]
+var officeApplication = [
+	{
+		name: "MS Word",
+		level: 40,
+	},
+	{
+		name: "MS Excel",
+		level: 60,
+	},
+	{
+		name: "Powerpoint",
+		level: 40,
+	}
+]
+module.exports.lenguages = lenguages;
+module.exports.officeApplication = officeApplication;
--- routes/index.js
+++ routes/index.js
@@ -1,51 +1,9 @@
 var express = require('express')
-
+var lenguages = require('models/').lenguages
+var officeApplication = require('models/').officeApplication
 var router = express.Router()
-var lenguages = [
-		{
-			name: "PHP",
-			level: 80,
-			description: "Probando men"
-		},
-		{
-			name: "jQuery",
-			level: 70,
-			description: "Probando men"
-		},
-		{
-			name: "HTML5",
-			level: "90",
-			description: "Probando men"
-		},
-		{
-			name: "CSS3",
-			level: "80",
-			description: "Probando men"
-		},
-		{
-			name: "Javascript",
-			level: "70",
-			description: "Probando men"
-		}
-	]
-var officeApplication = [
-	{
-		name: "MS Word",
-		level: 40,
-	},
-	{
-		name: "MS Excel",
-		level: 60,
-	},
-	{
-		name: "Powerpoint",
-		level: 40,
-	}
-]
-
 router.get('/', function (req, res) {
 	//var lenguages = [{ "name":"PHP", "level": "80" }, { "name": "jQuery", "level": "70" }]
 	res.render('index',{ title: 'Alver Grisales Ortega', lenguages : lenguages, officeApplication: officeApplication});
 })
-
 module.exports = router
